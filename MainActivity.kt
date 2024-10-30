33ebaa4da9a311d021189bfb0180fef294c209da
athiba <143354129+athibah@users.noreply.github.com>
2024-10-29 21:17:19 -0400

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.greetingcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.greetingcard.ui.theme.GreetingcardTheme
+import androidx.compose.material3.Text as Text1
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GreetingcardTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Magenta) {
+        Text1(
+            text = "Hello my name is $name!",
+            modifier = modifier
+        )
+    }
+}
+
+@Composable
+fun Surface(color: Color, content: @Composable () -> Unit) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GreetingcardTheme {
+        Greeting("Athina")
+    }
+}
